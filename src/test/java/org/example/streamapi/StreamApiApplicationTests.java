3c61b846a5f47fcc4c3ec376b7bcd2b93d087c08
shcherbina-nv <mailto:kolos.book@gmail.com>
2024-12-10 22:12:36 +0300

    first commit

---
+++ src/test/java/org/example/streamapi/StreamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.streamapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
