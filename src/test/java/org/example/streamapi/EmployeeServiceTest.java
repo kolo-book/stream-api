b9b0b28cb96f2f9ce52ee92d9c052a85f00e118e
shcherbina-nv <mailto:kolos.book@gmail.com>
2025-01-28 16:41:46 +0300

    homework mockito

---
+++ src/test/java/org/example/streamapi/EmployeeServiceTest.java
@@ -0,0 +1,36 @@
+package org.example.streamapi;
+
+import org.example.streamapi.service.DepartmentService;
+import org.example.streamapi.service.EmployeeService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class EmployeeServiceTest {
+
+    @Test
+    void testGetEmployeeMaxSalary() {
+        EmployeeService employeeServiceMock = Mockito.mock(EmployeeService.class);
+        DepartmentService departmentService = new DepartmentService(employeeServiceMock);
+
+        List<Employee> employees = List.of(
+                new Employee("John Doe", 1000, "1"),
+                new Employee("Jane Smith", 2000, "1"),
+                new Employee("Emily Davis", 1500, "2")
+        );
+
+        Mockito.when(employeeServiceMock.getAllEmployees()).thenReturn(employees);
+
+        Employee maxSalaryEmployee = departmentService.getEmployeeMaxSalary("1");
+        assertNotNull(maxSalaryEmployee);
+        assertEquals("Jane Smith", maxSalaryEmployee.getFullName());
+    }
+
+
+}
